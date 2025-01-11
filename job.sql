a95512f91917eebc4ef22ba8c2a09dca2afba65d
yahia kplr <yahiakplr@gmail.com>
2025-01-11 11:55:06 +0000

    sql

---
+++ job.sql
@@ -0,0 +1,14 @@
+CREATE TABLE persons
+(
+id serial NOT NULL,
+first_name character varying (50), 
+last_name character varying (50), 
+dob date,
+email character varying (255),
+CONSTRAINT persons_pkey PRIMARY KEY (id)
+)
+
+COPY persons(first_name, last_name, dob, email)
+FROM 'C:\tmp\persons.csv' DELIMITER ','CSV HEADER;
+
+TRUNCATE TABLE persons;
